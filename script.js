efcb2db23cbf24551954859ea071403ba9ad5e10
SutanjoyB <bsutanjoy@gmail.com>
2026-01-25 01:05:11 +0530

    Project Updated

---
+++ script.js
@@ -0,0 +1,44 @@
+console.log("Welcome to Spotify");
+
+// Initialize the variables
+let songIndex = 0;
+let audioElement = new Audio('1.mp3');
+let masterPlay = document.getElementById("masterPlay");
+let myProgressbar = document.getElementById("myProgressBar");
+let gif = document.getElementById("gif");
+
+
+let songs = [
+    {songName: "Salam-e-Ishq", filePath: "song/1.mp3", coverPath: "covers/1.jpg"},
+    {songName: "Salam-e-Ishq", filePath: "song/1.mp3", coverPath: "covers/1.jpg"},
+    {songName: "Salam-e-Ishq", filePath: "song/1.mp3", coverPath: "covers/1.jpg"},
+    {songName: "Salam-e-Ishq", filePath: "song/1.mp3", coverPath: "covers/1.jpg"},
+    {songName: "Salam-e-Ishq", filePath: "song/1.mp3", coverPath: "covers/1.jpg"},
+    {songName: "Salam-e-Ishq", filePath: "song/1.mp3", coverPath: "covers/1.jpg"},
+    {songName: "Salam-e-Ishq", filePath: "song/1.mp3", coverPath: "covers/1.jpg"},
+    {songName: "Salam-e-Ishq", filePath: "song/1.mp3", coverPath: "covers/1.jpg"},
+    {songName: "Salam-e-Ishq", filePath: "song/1.mp3", coverPath: "covers/1.jpg"},
+]
+// audioElement.play();
+
+// Handle Play/pause Click
+masterPlay.addEventListener('click', ()=>{
+    if(audioElement.paused || audioElement.currentTime <= 0){
+        audioElement.play();
+        masterPlay.classList.remove('fa-play-circle');
+        masterPlay.classList.add('fa-pause-circle');
+        gif.style.opacity = 1;
+    }
+    else{
+        audioElement.pause();
+        masterPlay.classList.remove('fa-pause-circle');
+        masterPlay.classList.add('fa-play-circle');
+        gif.style.opacity = 0;
+    }
+})
+
+// Listen to Events
+audioElement.addEventListener('timeupdate', ()=>{
+    console.log('timeupdate');
+    // Update Seekbar
+})
